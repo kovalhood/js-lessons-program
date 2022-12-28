fd18bf7accff36eb7b1f93e29d8407f839fc7d46
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-12-28 11:57:09 +0200

    Updated descriptions

--- js/08-example.js
+++ js/08-example.js
@@ -6,3 +6,5 @@
 // less than 17, output the string "Pending"
 // greater than or equal to 17 and less than or equal to 24, output the string "Expires"
 // greater than 24 , output the string "Overdue"
+
+// const hours = 10;
--- js/09-example.js
+++ js/09-example.js
@@ -5,3 +5,6 @@
 // If it's 1 day before the deadline - output the string "Tomorrow"
 // If it's 2 days before the deadline - output the string "The day after tomorrow"
 // If it's 3+ days before the deadline - print the string "Date in the future"
+
+// const daysUntilDeadline = 5;
+// // Write code below this line
